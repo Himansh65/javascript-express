5aba4752675f56b22415ebf4be50c87cf3e338b8
Himanshu <23f2005707@ds.study.iitm.ac.in>
2024-09-22 19:02:14 +0000

    conversion

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,16 @@
+let score = "ABC3342"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let result = 437483
+console.log(result);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true  and  0 => false
+// "" => "hitesh" => true
