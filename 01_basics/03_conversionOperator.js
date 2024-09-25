c7fdec92f83b06e085db0eace2c14fe03b85cce1
Himanshu <23f2005707@ds.study.iitm.ac.in>
2024-09-25 02:07:14 +0000

    compareDatatypes

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -9,8 +9,17 @@ console.log(valueInNumber);
 let result = 437483
 console.log(result);
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=> true  and  0 => false
 // "" => "hitesh" => true
+
+let str1 = "helllo"
+let str2 = "world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 + 2); // 122
+console.log(2 + 1 + "2"); // 32
